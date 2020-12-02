03f3e891b4729d3e93e511b1746f1bdc8bac26e4
Vince W <vincewallgren@me.com>
2020-12-02 04:12:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,17 @@ def batch_badge_creator(attendees)
     "Hello, my name is #{name}."
   end
 end
+
+def assign_rooms(attendees)
+  attendees.each_with_index.map do |attendee, index|
+  "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+end
+end
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |assignment|
+     puts assignment
+   end
+ end
