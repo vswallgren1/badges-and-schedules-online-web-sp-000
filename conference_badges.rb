a4d52378d70fc830118d08fc98eee071d53826cb
Vince W <vincewallgren@me.com>
2020-12-02 04:11:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,4 +2,6 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator(attendees)
+  attendees.each do |name|
+    "Hello, my name is #{name}."
